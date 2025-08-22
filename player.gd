b2555cbece39cf2c60cc67659080c2201c363adc
Micha Albert <micha@2231puppy.tech>
2025-08-22 18:54:29 -0400

    cleanup

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 350.0
 const JUMP_VELOCITY = -400.0
 
+var has_enemy_been_removed = false
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,6 +21,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
-		if collision.get_collider().name == "win":
-			print("win!")
+		if collision.get_collider().name == "win" and not has_enemy_been_removed:
+			has_enemy_been_removed = true
 			get_parent().remove_child(get_parent().get_node("Enemy"))
